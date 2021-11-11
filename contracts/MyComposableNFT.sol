23d9d879cb146624e42fa9fb57dfaf9aaf4cb633
Kazune Takeda <58710601+kazunetakeda25@users.noreply.github.com>
2021-11-11 20:09:54 +0900

    create first commit without test

--- contracts/MyComposableNFT.sol
+++ contracts/MyComposableNFT.sol
@@ -6,10 +6,8 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./interfaces/IERC998TopDownERC20.sol";
 
-
 // TODO: layer in composability for ERC20 tokens based on EIP-998 (Top Down ERC20 specifically)
 contract MyComposableNFT is ERC721("MyComposable", "MYC"), IERC998TopDownERC20 {
-
     // State Variables
     using Address for address;
 
@@ -22,23 +20,56 @@ contract MyComposableNFT is ERC721("MyComposable", "MYC"), IERC998TopDownERC20 {
         _mint(_recipient, _tokenId);
     }
 
-    function balanceOfERC20(uint256 _tokenId, address _erc20Contract) public view override returns (uint256) {
+    function balanceOfERC20(uint256 _tokenId, address _erc20Contract)
+        public
+        view
+        override
+        returns (uint256)
+    {
         return erc20Balances[_tokenId][_erc20Contract];
     }
 
-    function erc20ContractByIndex(uint256 _tokenId, uint256 _index) public view override returns (address) {
-        require(_index < erc20Contracts[_tokenId].length, "Contract address does not exist for this token and index.");
-        
+    function erc20ContractByIndex(uint256 _tokenId, uint256 _index)
+        public
+        view
+        override
+        returns (address)
+    {
+        require(
+            _index < erc20Contracts[_tokenId].length,
+            "Contract address does not exist for this token and index."
+        );
+
         return erc20Contracts[_tokenId][_index];
     }
 
-    function totalERC20Contracts(uint256 _tokenId) public view override returns (uint256) {
+    function totalERC20Contracts(uint256 _tokenId)
+        public
+        view
+        override
+        returns (uint256)
+    {
         return erc20Contracts[_tokenId].length;
     }
 
-    function transferERC20(uint256 _tokenId, address _to, address _erc20Contract, uint256 _value) external override {
+    function transferERC20(
+        uint256 _tokenId,
+        address _to,
+        address _erc20Contract,
+        uint256 _value
+    ) external override {
         require(_to != address(0), "Can't transfer to zero address.");
-        require(_isApprovedOrOwner(msg.sender, _tokenId), "The caller of ERC721 transfer must be owner or must be approved.");
+        require(
+            _isApprovedOrOwner(msg.sender, _tokenId),
+            "The caller of ERC721 transfer must be owner or must be approved."
+        );
+
+        removeERC20(_tokenId, _erc20Contract, _value);
+
+        require(
+            IERC20AndERC223(_erc20Contract).transfer(_to, _value),
+            "ERC20 transfer failed."
+        );
 
         emit TransferERC20(_tokenId, _to, _erc20Contract, _value);
     }
@@ -50,41 +81,53 @@ contract MyComposableNFT is ERC721("MyComposable", "MYC"), IERC998TopDownERC20 {
         uint256 _value,
         bytes calldata _data
     ) external override {
-        require(_to != address(0), "ERC20 transfer to the zero address");
-        require(_isApprovedOrOwner(msg.sender, _tokenId));
+        require(_to != address(0), "Can't transfer to zero address.");
+        require(
+            _isApprovedOrOwner(msg.sender, _tokenId),
+            "The caller of ERC721 transfer must be owner or must be approved."
+        );
+
         removeERC20(_tokenId, _erc223Contract, _value);
-        require(IERC20AndERC223(_erc223Contract).transfer(_to, _value, _data), "ERC223 transfer failed.");
+
+        require(
+            IERC20AndERC223(_erc223Contract).transfer(_to, _value, _data),
+            "ERC223 transfer failed."
+        );
+
         emit TransferERC20(_tokenId, _to, _erc223Contract, _value);
     }
 
-    // this contract has to be approved first by _erc20Contract
     function getERC20(
         address _from,
         uint256 _tokenId,
         address _erc20Contract,
         uint256 _value
     ) public override {
-        require(_from == msg.sender, "Not allowed to getERC20");
+        require(_from == msg.sender, "Not allowed to get ERC20");
+
         erc20Received(_from, _tokenId, _erc20Contract, _value);
-        
+
         require(
-            IERC20AndERC223(_erc20Contract).transferFrom(_from, address(this), _value),
+            IERC20AndERC223(_erc20Contract).transferFrom(
+                _from,
+                address(this),
+                _value
+            ),
             "ERC20 transfer failed."
         );
     }
 
-    // used by ERC 223
     function tokenFallback(
         address _from,
         uint256 _value,
         bytes calldata _data
     ) external override {
-        require(_data.length > 0, "_data must contain the uint256 tokenId to transfer the token to.");
+        require(
+            _data.length > 0,
+            "The length of token data should larger than zero."
+        );
         require(Address.isContract(msg.sender), "msg.sender is not a contract");
-        /**************************************
-         * TODO move to library
-         **************************************/
-        // convert up to 32 bytes of_data to uint256, owner nft tokenId passed as uint in bytes
+
         uint256 tokenId;
         assembly {
             tokenId := calldataload(132)
@@ -92,7 +135,7 @@ contract MyComposableNFT is ERC721("MyComposable", "MYC"), IERC998TopDownERC20 {
         if (_data.length < 32) {
             tokenId = tokenId >> (256 - _data.length * 8);
         }
-        //END TODO
+
         erc20Received(_from, tokenId, msg.sender, _value);
     }
 
@@ -102,16 +145,20 @@ contract MyComposableNFT is ERC721("MyComposable", "MYC"), IERC998TopDownERC20 {
         address _erc20Contract,
         uint256 _value
     ) private {
-        require(_exists(_tokenId), "_tokenId does not exist.");
+        require(_exists(_tokenId), "The tokenId does not exist.");
+
         if (_value == 0) {
             return;
         }
         uint256 erc20Balance = erc20Balances[_tokenId][_erc20Contract];
         if (erc20Balance == 0) {
-            erc20ContractIndex[_tokenId][_erc20Contract] = erc20Contracts[_tokenId].length;
+            erc20ContractIndex[_tokenId][_erc20Contract] = erc20Contracts[
+                _tokenId
+            ].length;
             erc20Contracts[_tokenId].push(_erc20Contract);
         }
         erc20Balances[_tokenId][_erc20Contract] = erc20Balance + _value;
+
         emit ReceivedERC20(_from, _tokenId, _erc20Contract, _value);
     }
 
@@ -124,14 +171,18 @@ contract MyComposableNFT is ERC721("MyComposable", "MYC"), IERC998TopDownERC20 {
             return;
         }
         uint256 erc20Balance = erc20Balances[_tokenId][_erc20Contract];
-        require(erc20Balance >= _value, "Not enough token available to transfer.");
+
+        require(erc20Balance >= _value, "Don't have enough token to transfer.");
+
         uint256 newERC20Balance = erc20Balance - _value;
         erc20Balances[_tokenId][_erc20Contract] = newERC20Balance;
         if (newERC20Balance == 0) {
             uint256 lastContractIndex = erc20Contracts[_tokenId].length - 1;
             address lastContract = erc20Contracts[_tokenId][lastContractIndex];
             if (_erc20Contract != lastContract) {
-                uint256 contractIndex = erc20ContractIndex[_tokenId][_erc20Contract];
+                uint256 contractIndex = erc20ContractIndex[_tokenId][
+                    _erc20Contract
+                ];
                 erc20Contracts[_tokenId][contractIndex] = lastContract;
                 erc20ContractIndex[_tokenId][lastContract] = contractIndex;
             }
--- contracts/interfaces/IERC998TopDownERC20.sol
+++ contracts/interfaces/IERC998TopDownERC20.sol
@@ -3,12 +3,11 @@ pragma solidity 0.7.6;
 // SPDX-License-Identifier: MIT
 
 interface IERC998TopDownERC20 {
-
-// Events
+    // Events
     event TransferERC20(
-        uint256 indexed _tokenId, 
-        address indexed _to, 
-        address indexed _erc20Contract, 
+        uint256 indexed _tokenId,
+        address indexed _to,
+        address indexed _erc20Contract,
         uint256 _value
     );
 
@@ -19,26 +18,65 @@ interface IERC998TopDownERC20 {
         uint256 _value
     );
 
-// Functions
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+    // Functions
+    function balanceOfERC20(uint256 _tokenId, address __erc20Contract)
+        external
+        view
+        returns (uint256);
 
-    function transfer(address to, uint256 value) external returns (bool success);
+    function transferERC20(
+        uint256 _tokenId,
+        address _to,
+        address _erc20Contract,
+        uint256 _value
+    ) external;
 
-    function transfer(address to, uint256 value, bytes memory data) external returns (bool success);
+    function transferERC223(
+        uint256 _tokenId,
+        address _to,
+        address _erc223Contract,
+        uint256 _value,
+        bytes memory _data
+    ) external;
 
-    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+    function getERC20(
+        address _from,
+        uint256 _tokenId,
+        address _erc20Contract,
+        uint256 _value
+    ) external;
+
+    function tokenFallback(
+        address _from,
+        uint256 _value,
+        bytes memory _data
+    ) external;
+}
+
+interface IERC998ERC20TopDownEnumerable {
+    // Functions
+    function totalERC20Contracts(uint256 _tokenId)
+        external
+        view
+        returns (uint256);
+
+    function erc20ContractByIndex(uint256 _tokenId, uint256 _index)
+        external
+        view
+        returns (address);
 }
 
 interface IERC20AndERC223 {
-
-// Functions
+    // Functions
     function transferFrom(
         address _from,
         address _to,
         uint256 _value
     ) external returns (bool success);
 
-    function transfer(address to, uint256 value) external returns (bool success);
+    function transfer(address to, uint256 value)
+        external
+        returns (bool success);
 
     function transfer(
         address to,
@@ -46,5 +84,8 @@ interface IERC20AndERC223 {
         bytes memory data
     ) external returns (bool success);
 
-    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+    function allowance(address _owner, address _spender)
+        external
+        view
+        returns (uint256 remaining);
 }
