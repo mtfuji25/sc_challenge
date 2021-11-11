00fa187d3fe622fb4d89e0989f22e83dcbf71f32
Kazune Takeda <58710601+kazunetakeda25@users.noreply.github.com>
2021-11-11 19:56:40 +0900

    ERC223 required

--- contracts/interfaces/IERC998TopDownERC20.sol
+++ contracts/interfaces/IERC998TopDownERC20.sol
@@ -2,8 +2,6 @@ pragma solidity 0.7.6;
 
 // SPDX-License-Identifier: MIT
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
 interface IERC998TopDownERC20 {
 
 // Events
@@ -27,6 +25,26 @@ interface IERC998TopDownERC20 {
     function transfer(address to, uint256 value) external returns (bool success);
 
     function transfer(address to, uint256 value, bytes memory data) external returns (bool success);
+
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+}
+
+interface IERC20AndERC223 {
+
+// Functions
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) external returns (bool success);
+
+    function transfer(address to, uint256 value) external returns (bool success);
+
+    function transfer(
+        address to,
+        uint256 value,
+        bytes memory data
+    ) external returns (bool success);
 
     function allowance(address _owner, address _spender) external view returns (uint256 remaining);
 }
